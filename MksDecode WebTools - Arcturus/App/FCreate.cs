14bd75fdd284744c7e1d6f336c2ab6b4394206cd
Okami ラコステ <58578838+OkamiMks@users.noreply.github.com>
2021-09-04 15:11:05 -0300

    Add files via upload

---
+++ MksDecode WebTools - Arcturus/App/FCreate.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+
+namespace App
+{
+    public class FCreate
+    {
+        public static string Dir { get; set; }
+        public static string Config { get; set; }
+        public static string Path { get; set; }
+        public string PathReturn { get; set; }
+
+
+        public FCreate(string dir, string config, string path)
+        {
+            Dir = dir;
+            Config = config;
+            Path = path;
+        }
+        public void FolderCreate()
+        {
+            try
+            {
+                if (!Directory.Exists(Dir)) Directory.CreateDirectory(Dir);
+                else if (!Directory.Exists(Config)) Directory.CreateDirectory(Config);
+                else if (!File.Exists(Path)) File.Create(Path);
+            } catch { }
+
+            try { if (File.Exists(Path)) PathReturn = Path; } catch { }
+        }
+        public string writePath()
+        {
+            return this.PathReturn;
+        }
+    }
+    public class ProxyCreateFolder
+    {
+        public static string Dir { get; set; }
+        public static string Config { get; set; }
+        public static string Path { get; set; }
+        public string PathReturn { get; set; }
+
+        public ProxyCreateFolder(string dir, string config, string path)
+        {
+            Dir = dir;
+            Config = config;
+            Path = path;
+        }
+
+        public void pFolderCreate()
+        {
+            try
+            {
+                if (!Directory.Exists(Dir)) Directory.CreateDirectory(Dir);
+                else if (!Directory.Exists(Config)) Directory.CreateDirectory(Config);
+                else if (!File.Exists(Path)) File.Create(Path);
+            } catch { }
+
+            try { if (File.Exists(Path)) PathReturn = Path; } catch { }
+        }
+        public string writePathProxy()
+        {
+            return this.PathReturn;
+        }
+    }
+}
---
+++ MksDecode WebTools - Arcturus/App/List.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace App
+{
+    public class List
+    {
+        public List<string> listS = new List<string>();
+
+        public void addToList(string[] content)
+        {
+            foreach (var i in content)
+                listS.Add(i);
+        }
+    }
+}
