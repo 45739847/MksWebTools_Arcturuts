242edae2ff6f4a6138db0eea070d8ff0840f7c41
Okami ラコステ <58578838+OkamiMks@users.noreply.github.com>
2021-09-04 15:20:47 -0300

    Add files via upload

---
+++ MksDecode WebTools - Arcturus/App/ListKeys.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace App
+{
+    public class ListKeys
+    {
+        public string[] KeyReturns;
+        public int contLines { get; set; }
+
+        public void openList(string path)
+        {
+            string[] returnList;
+            if (File.Exists(path))
+            {
+                returnList = Convert.ToString(File.ReadAllText(path)).Split('\n');
+                KeyReturns = returnList;
+            }
+        }
+    }
+}
